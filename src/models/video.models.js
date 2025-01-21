5e3416e3083b35c525719a4579469c631363bf86
vaibhav-kadam3107 <vaibhav.kadam21@vit.edu>
2025-01-21 18:32:47 +0530

    add mongoose-aggregate-paginate-v2 plugin to video model for pagination support

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,4 +1,5 @@
 import mongoose , { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema ({
 
@@ -34,5 +35,7 @@ const videoSchema = new Schema ({
         ref: "User"
     }
 } , {timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema);
