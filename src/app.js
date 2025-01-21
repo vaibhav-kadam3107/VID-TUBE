657f958ec431c1e127d19ae101213f44af7104eb
vaibhav-kadam3107 <vaibhav.kadam21@vit.edu>
2025-01-21 23:34:29 +0530

    add user model methods for password encryption and token generation; include cookie-parser dependency

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
 
 const app = express()
 
@@ -16,6 +18,9 @@ app.use(
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true , limit: '16kb'}))
 app.use(express.static('public'))
+
+// for cookies
+app.use(cookieParser())
 
 
 // import routes
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose , { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj -  ERD
 
@@ -48,5 +50,28 @@ const userSchema = new Schema({
     }
 
 }, {timestamps: true})
+
+// encrypt the password before saving the user
+userSchema.pre("save", async function(next) {
+    if(this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+})
+
+// compare the password
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return bcrypt.compare(password, this.password);
+}
+
+// generate the access token
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign({id: this._id, email : this.email, username: this.username, fullname: this.fullname}, process.env.ACCESS_JWT_SECRET, {expiresIn: process.env.ACCESS_TOKEN_EXPIRY});
+}
+
+// generate the refresh token
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign({id: this._id}, process.env.REFRESH_JWT_SECRET, {expiresIn: process.env.REFRESH_TOKEN_EXPIRY});
+}
 
 export const User = mongoose.model("User", userSchema);
